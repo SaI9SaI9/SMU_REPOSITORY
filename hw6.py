6d56e50897f461b3d74012db1ca27bc7a7f75219
sai9 <166096796+SaI9SaI9@users.noreply.github.com>
2025-04-15 22:35:29 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,12 @@
+for i in range(1, 10, 1):
+    for n in range(2, 6, 1):
+            print(f'{n} X {i} = {n*i:>2d}', end='   ')
+    print()
+
+print()
+
+for i in range(1, 10, 1):
+    for n in range(6, 10, 1):
+            print(f'{n} X {i} = {n*i:>2d}', end='   ')
+    print()
+    
